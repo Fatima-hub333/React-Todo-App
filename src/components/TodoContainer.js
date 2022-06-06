36ab8089395bec8417510e91897ac38419cff7a8
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-06 22:45:13 +0500

    Update Parent file (TodoContainer) of Components

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,13 @@
+import React from "react"
+class TodoContainer extends React.Component {
+  render() {
+    return (
+      <div> 
+        <h1> Hello From Create React APP</h1>
+        <p>I am in a React Component!</p>
+      </div>
+    )
+  }
+}
+
+export default TodoContainer
